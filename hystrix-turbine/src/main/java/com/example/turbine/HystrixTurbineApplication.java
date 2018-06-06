f59c1e0a01f9c3ff718c2b6d4c0586b933bab1b5
357932982 <357932982@qq.com>
2018-06-06 18:33:28 +0800

    update hystrix-turbine project

--- hystrix-turbine/src/main/java/com/example/turbine/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/example/turbine/HystrixTurbineApplication.java
@@ -2,10 +2,12 @@ package com.example.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine  //激活对turbine的支持
+@EnableHystrixDashboard
 public class HystrixTurbineApplication {
 
 	public static void main(String[] args) {
