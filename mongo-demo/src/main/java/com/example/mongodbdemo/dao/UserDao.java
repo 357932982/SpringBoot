26f18fea8bf48d8fae54c18f5ad0d98b52251dfd
y357932982 <357932982@qq.com>
2018-06-20 23:45:23 +0800

    learned mongodb

---
+++ mongo-demo/src/main/java/com/example/mongodbdemo/dao/UserDao.java
@@ -0,0 +1,70 @@
+package com.example.mongodbdemo.dao;
+
+import com.example.mongodbdemo.entity.User;
+import com.mongodb.client.result.UpdateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserDao {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 创建对象
+     * @param user: user对象
+     */
+    public void saveUser(User user){
+        mongoTemplate.save(user);
+    }
+
+    /**
+     * 根据用户名查询
+     * @param name
+     * @return
+     */
+    public User findUserByName(String name){
+        Query query = new Query(Criteria.where("name").is(name));
+        User user = mongoTemplate.findOne(query, User.class);
+        return user;
+    }
+
+    /**
+     * 更新对象
+     * @param user
+     * @return
+     */
+    public String updateUser(User user){
+        Query query = new Query(Criteria.where("id").is(user.getId()));
+        Update update = new Update().set("name", user.getName()).set("password", user.getPassword());
+        //更新查询返回结果集的第一条
+        UpdateResult result = mongoTemplate.updateFirst(query, update, User.class);
+        //更新查询返回结果的所有
+        //mongoTemplate.updateMulti(query, update, User.class);
+        return result.toString();
+    }
+
+    /**
+     * 根据ID删除
+     * @param id
+     * @return
+     */
+    public String updateUserById(Integer id){
+        Query query = new Query(Criteria.where("id").is(id));
+        Update update = new Update().set("name", "花花");
+        UpdateResult result = mongoTemplate.updateFirst(query, update, User.class);
+        return result.toString();
+    }
+
+    public void deleteUserById(Integer id){
+        Query query = new Query(Criteria.where("id").is(id));
+        mongoTemplate.remove(query, User.class);
+    }
+
+}
---
+++ mongo-demo/src/test/java/com/example/mongodbdemo/MongodbDemoApplicationTests.java
@@ -0,0 +1,55 @@
+package com.example.mongodbdemo;
+
+import com.example.mongodbdemo.dao.UserDao;
+import com.example.mongodbdemo.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongodbDemoApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setId(1);
+        user.setName("小明");
+        user.setPassword("123456");
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void testFind(){
+        User user = userDao.findUserByName("小明");
+        System.out.println("user:"+user);
+
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(1);
+        user.setName("静静");
+        user.setPassword("654321");
+        String s = userDao.updateUser(user);
+        System.out.println(s);
+    }
+
+    @Test
+    public void testUpdateById(){
+        String s = userDao.updateUserById(1);
+        System.out.println(s);
+    }
+
+    @Test
+    public void testDelete(){
+        userDao.deleteUserById(1);
+    }
+
+}
